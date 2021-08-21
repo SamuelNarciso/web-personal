7987f00ca17ec1886be87381ae9d44916cafb09b
samuelnarciso <samuelnarciso3280@gmail.com>
2021-08-20 23:49:00 -0500

    Arreglos en el acerca de

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 import Header from '../components/header';
@@ -12,6 +11,7 @@ import libros from '../images/svg/icons/libros.svg';
 import correr from '../images/svg/icons/correr.svg';
 import gamepad from '../images/svg/icons/gamepad.svg';
 import swim from '../images/svg/icons/swim.svg';
+import { Link } from 'gatsby';
 
 const IndexPage = ({ data }) => (
   <>
@@ -20,26 +20,42 @@ const IndexPage = ({ data }) => (
       <Header titulo="Acerca de" />
       <p className="regular parrafo">
         Hola ✋. Me llamo <span className="bold"> Samuel Narciso </span> , soy
-        un desarrollador web orientado al{' '}
-        <span className="resaltado"> frontend </span>, apasionado de distintas
-        tecnologias .
-        <br /> Me gusta experimentar o{' '}
-        <span className="resaltado"> jugar </span> con distintas tecnologias asi
-        como aprender cosas nuevas sobre ellas, la experiencia actual con la que
-        cuento es con <span className="resaltado"> proyectos </span> que he
-        realizado en la escuela y algunos que he realizado por cuenta propia. Me
-        gusta crear contenido y de esta manera{' '}
-        <span className="resaltado"> enseñar </span> a otros desarrolladores, ya
-        sea mediante post de mi blog o videos de youtube.
-        <br /> Con la tecnologia que me siento comodo trabajando es con
-        tecnologias de <span className="resaltado"> Javascript </span> como
-        react y NodeJS.
-        <br /> Actualmente estoy estudiando una ingenieria en sistemas
-        computacionales en el instituto tecnologico nacional de México campus
-        Cuautla.
+        un desarrollador web orientado al frontend, apasionado a distintas
+        tecnologias de desarrollo.
       </p>
-      <br/>
-    <p className='subtitulo'> Aficiones y pasatiempos </p>
+      <p className="regular parrafo">
+        Me gusta experimentar o jugar con distintas tecnologias asi como
+        aprender cosas nuevas sobre ellas, la experiencia actual con la que
+        cuento es con proyectos que he realizado en la escuela y algunos que he
+        realizado por cuenta propia.
+      </p>
+      <p className="regular parrafo">
+        Me gusta crear contenido y de esta manera enseñar a otros
+        desarrolladores, mediante {' '}
+        <Link className="bold texto-link" to="/blog">
+          post en mi blog {' '}
+        </Link>
+         o 
+        <a
+          className="bold texto-link"
+          href="https://www.youtube.com/channel/UCdlswAjW13BPfV9jo5VLJnQ"
+          alt="canal de youtube samuel narciso"
+        >
+           {' '}videos de youtube
+        </a>
+        .
+      </p>
+      <p className="regular parrafo">
+        Con las tecnologias que me siento comodo trabajando es con tecnologias
+        de Javascript tales como react y NodeJS.
+      </p>
+
+      <p className="regular parrafo">
+        Actualmente estoy estudiando una ingenieria en sistemas computacionales
+        en el instituto tecnologico nacional de México campus Cuautla.
+      </p>
+      <br />
+      <p className="subtitulo"> Aficiones y pasatiempos </p>
       <div className="container-wrap">
         <Card
           icon={cocina}
@@ -47,22 +63,20 @@ const IndexPage = ({ data }) => (
           titulo="Cocinar"
           texto=""
         />
-          <Card
+        <Card
           icon={correr}
           alt="Correr como pasatiempo"
           titulo="Correr"
           texto=""
         />
-   
-         <Card
+
+        <Card
           icon={libros}
           alt="Estudiar como pasatiempo"
           titulo="Estudiar"
           texto=""
         />
-       
-       
-      
+
         <Card
           icon={gamepad}
           alt="Aficionado a los videojuegos"
@@ -75,28 +89,15 @@ const IndexPage = ({ data }) => (
           titulo="Nadar"
           texto=""
         />
-    <Card
+        <Card
           icon={coding}
           alt="Aficionado a la programacion"
           titulo="Programar"
           texto=""
         />
       </div>
-      {/* <h3 className="subtitulo bold t-left">Tecnologias con las que he trabajado</h3> */}
     </Layout>
   </>
 );
-
-// export const query = graphql`
-//   {
-//     allProjectsJson {
-//       nodes {
-//         titulo
-//         tecnologias
-//         fecha
-//       }
-//     }
-//   }
-// `;
 
 export default IndexPage;
